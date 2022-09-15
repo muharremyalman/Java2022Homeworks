ce45df276134cb682d514ce609339d4c37d93d10
muharremyalman <yalmanmuharrem507@gmail.com>
2022-09-15 16:35:25 +0300

    İf Bloklarıyla Çalışmak

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 24;
+		if (sayi<20) { //Eğer sayı 20 den küçükse, bu şart doğruysa
+			System.out.println("Sayı 20'den küçüktür"); //bu blog çalışır
+		}else if(sayi==20) { // == Eşit mi ? 
+			System.out.println("Sayı 20'ye eşittir");  //bu blog çalışır
+		}else {//Farklıysa şart dışındaysa diğer durumlar
+			System.out.println("Sayı 20'den büyüktür"); // bu blog çalışır.
+		}
+		
+		//Savunmacı Programlama
+
+	}
+
+}
