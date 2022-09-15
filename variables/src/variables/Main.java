c91f271cc30ea15e21574b2a7c99626d53ddaeb6
muharremyalman <yalmanmuharrem507@gmail.com>
2022-09-15 15:42:17 +0300

    Değişkenler ve kullanım ihtiyacı

--- variables/src/variables/Main.java
+++ variables/src/variables/Main.java
@@ -3,8 +3,20 @@ package variables;
 public class Main {
 
 	public static void main(String[] args) {
-		//case sensitive
-		System.out.println("Öğrenci sayım : 9");
+		//Camel Casing (İlk kelime küçük sonrası büyük deve şeklinden dolayı)
+		//case sensitive (Büyük küçük harf duyarlı)
+		int ogrenciSayisi = 12;
+		String mesaj = "Öğrenci sayısı : ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		
+		//System.out.println("Öğrenci sayısı : "+ ogrenciSayisi);
+		//System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+		//System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+		//System.out.println("Öğrenci sayım : "+ ogrenciSayisi);
+		//reusability (Bir kere yaz her yerde kullan, kod tekrarından ve bellekten tasarruf için değişken  kullanılır)
+		
+		
 
 	}
 
