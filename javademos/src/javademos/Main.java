4dcf3194b6cc179aa7139dbdff21d80812f07cfe
muharremyalman <yalmanmuharrem507@gmail.com>
2022-09-15 15:20:06 +0300

    Hello Word

---
+++ javademos/src/javademos/Main.java
@@ -0,0 +1,13 @@
+package javademos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//intellisense Eclipse IDE ctrl + space
+		
+		System.out.println("Merhaba Java");
+		System.out.println("Merhaba Java 2");
+		
+	}
+
+}
