959294b4f0fdda2d0e6364afb89c3c24651c3ca8
muharremyalman <yalmanmuharrem507@gmail.com>
2022-09-15 17:23:44 +0300

    For ve While Döngüsüyle Çalışmak

--- loopDemo/src/loopDemo/Main.java
+++ loopDemo/src/loopDemo/Main.java
@@ -7,7 +7,15 @@ public class Main {
 		for(int i=1;i<=10;i++) {//i=i+1 veya i++ bir arttır demektir. Sayaç 1 den başla; i 10 dan küçük olduğu sürece; birer arttır; i+=2 2şer arttır vs.
 			System.out.println(i); // i 10'a gelene kadar döngü devam eder, tekrar gider 11 10dan büyük mü döngü dışına çıkar kod varsa çalışmaya devam eder.
 		}
-		System.out.println("Döngü Bitti");
+		System.out.println("For Döngüsü Bitti");
+		
+		int i = 1;
+		//While
+		while(i<10) {
+			System.out.println(i);
+			i++; // i+=2 tek sayıları yazdırırdım. 1den 9a kadar sayar 
+		}
+			System.out.println("While Döngüsü Bitti");
 	}
 
 }
