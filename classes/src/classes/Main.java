800d2a4d83f519b4ff43d9605ae772abbccc4c17
muharremyalman <yalmanmuharrem507@gmail.com>
2022-09-22 19:25:14 +0300

    değer ve referans tipleri anlamak

--- classes/src/classes/Main.java
+++ classes/src/classes/Main.java
@@ -10,7 +10,20 @@ public class Main {
 		customerManager.Add();
 		customerManager.Remove();
 		customerManager.Update();
-
+		
+		//value type değer tipleride stack yazılır referans tipte stack hesap geçer
+		int sayi1 = 10;
+		int sayi2 = 20;
+		sayi2=sayi1;
+		sayi1=30;
+		System.out.println(sayi2);
+		
+		int[] sayilar1 = new int [] {1,2,3}; //diziler referans tiptir
+		int[] sayilar2 = new int [] {4,5,6};
+		sayilar2 = sayilar1;
+		sayilar1[0]=10;
+		System.out.println(sayilar2[0]);
+		
 	}
 	
 	
