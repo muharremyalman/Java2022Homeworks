822ed27f82760f64c469c9f044ca84825f11c009
muharremyalman <yalmanmuharrem507@gmail.com>
2022-09-22 21:02:41 +0300

    getter ve setter ile çalışmak

--- classesWithAttributes/src/classesWithAttributes/Main.java
+++ classesWithAttributes/src/classesWithAttributes/Main.java
@@ -3,19 +3,18 @@ package classesWithAttributes;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product = new Product();
-		product.name = "Laptop";
-		product.id = 1;
-		product.description="Asus Laptop";
-		product.price = 5000;
-		product.stockAmount=3;
-	
-		
-		ProductManager productManager = new ProductManager();
-		productManager.Add(product);
-		
-		
+		// Product product = new Product(1 , "Laptop" , "Monster Abra" , 15000 , 2 );
+				Product product = new Product();
+				product.setName("Laptop"); 
+				product.setId(1);
+				product.setDescription("Monster Abra");
+				product.setPrice(15000);
+				product.setStockAmount(3);
+				
+				ProductManager productManager = new ProductManager();
+				productManager.add(product);
+				System.out.println(product.getCode());
 
-	}
+			}
 
-}
+		}
--- classesWithAttributes/src/classesWithAttributes/ProductManager.java
+++ classesWithAttributes/src/classesWithAttributes/ProductManager.java
@@ -1,12 +1,8 @@
 package classesWithAttributes;
 
 public class ProductManager {
-	public void Add(Product product) {
-		//JDBC
-		System.out.println("Ürün eklendl" + product.name);
+	public void add(Product product) {
+		System.out.println("Product added: "+product.getName());
 	}
-	
-	public void Add2(int id, String name, String description, int stockAmount, double price) {
-		
-	}
+
 }
